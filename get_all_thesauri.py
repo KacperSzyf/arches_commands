c36ffc2228caf981779fb589018e9a95a3b3e2fb
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2021-12-21 17:02:34 +0000

    Removed old comments

--- get_all_thesauri.py
+++ get_all_thesauri.py
@@ -1,24 +1,3 @@
-# from django.views import View
-# from arches.app.models.models import Concept as modelConcept
-# from arches.app.models.concept import Concept
-# from arches.app.utils.skos import SKOSWriter
-# from django.http import HttpResponse
-
-# class ConceptsExportView(View):
-#     def get(self, request):
-#         conceptids = [str(c.conceptid) for c in modelConcept.objects.filter(nodetype='ConceptScheme')]
-#         concept_graphs = []
-#         for conceptid in conceptids:
-#             print(conceptid)
-#             concept_graphs.append(Concept().get(
-#                 id=conceptid,
-#                 include_subconcepts=True,
-#                 include_parentconcepts=False,
-#                 include_relatedconcepts=True,
-#                 depth_limit=None,
-#                 up_depth_limit=None))
-#         return HttpResponse(SKOSWriter().write(concept_graphs, format="pretty-xml"), content_type="application/xml")
-
 #Imports
 import os
 from django.core.management.base import BaseCommand, CommandError
